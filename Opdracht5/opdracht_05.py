0346bd4c4e66df46228f1e90bc66e0a8dd8842c3
jayden van der bijl <2158035@talnet.nl>
2021-11-01 13:06:46 +0100

    modular code

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,9 @@
-#maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
-#elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+def favorite_dingen(name):
+    print("mijn favorite dingen", name)
+
+favorite_dingen("mijn favorite game is riders republic")
+favorite_dingen("mijn favo eten is zuurkool met rookworst")
+favorite_dingen("de sport waar ik op zit is zwemmen")
+
+  
+
