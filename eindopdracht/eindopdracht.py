f1047ece74f656dcdb561885c61dab22b1a9dac0
jayden van der bijl <2158035@talnet.nl>
2021-11-02 09:26:45 +0100

    thinkcode

---
+++ eindopdracht/eindopdracht.py
@@ -0,0 +1,16 @@
+def favorite_dingen(name):
+    print("mijn favorite dingen", name)
+
+favorite_dingen("mijn favorite game is riders republic")
+favorite_dingen("mijn favo eten is zuurkool met rookworst")
+favorite_dingen("de sport waar ik op zit is zwemmen")
+
+  
+print("hi")
+name=input("are you enjoying the course")
+
+if answer=="yes":
+    print("that's good to hear!")
+else: 
+    print("oh no! that makes me sad!")
+
